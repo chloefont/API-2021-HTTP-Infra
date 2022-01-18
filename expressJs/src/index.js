44ed2f67ce47c7ffb0cfb2bfd4451259cd05b0c4
chloefont <cfontaine@citycable.ch>
2022-01-18 16:02:40 +0100

    load balancing ok

--- expressJs/src/index.js
+++ expressJs/src/index.js
@@ -5,10 +5,14 @@ app.get("/", (req, res) => {
     res.json(getTimeJson());
 });
 
-app.get("/id", (req, res) => {
-    console.log(process.env);
-    res.json({id:process.env.NO_SERVER});
-});
+// app.get("/id", (req, res) => {
+//     console.log(process.env);
+//     res.json({id:process.env.NO_SERVER});
+// });
+
+// app.get('*', (req, res)=>{
+//     console.log(req.url);
+// })
 
 app.listen(3006, ()=>{
     console.log("Server started")
@@ -17,13 +21,16 @@ app.listen(3006, ()=>{
 
 function getTimeJson() {
     var now = new Date();
+
+    console.log(process.env.NO_SERVER)
     return {
         year: now.getFullYear(),
         month: now.getMonth(),
         day: now.getDay(),
         hour: now.getHours(),
         minutes: now.getMinutes(),
-        seconds: now.getSeconds()
+        seconds: now.getSeconds(),
+        noServer: process.env.NO_SERVER
         };
 }
 
